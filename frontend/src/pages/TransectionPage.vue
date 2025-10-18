3ad3357106da838687c92278950c488cd7a53ba8
Fahim-Foysal-Apurba <fahim.apurba@northsouth.edu>
2025-10-18 22:24:33 +0600

    Add frontend app (without node_modules)

---
+++ frontend/src/pages/TransectionPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="transection-page">
+        <h1>Transection Page</h1>
+        <!-- Transection page content goes here -->
+    </div>
+</template>
+<script setup>
+
+</script>
+
+<style>
+</style>
