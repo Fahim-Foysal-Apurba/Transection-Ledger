d3eb0db2602577b5c1689819d32dbf902159d8d5
Fahim-Foysal-Apurba <fahim.apurba@northsouth.edu>
2025-10-19 23:51:58 +0600

    4 new apis in backend

---
+++ backend/Apis.js
@@ -0,0 +1,102 @@
+import express from 'express';
+import pool from './DB.js';
+
+const router= express.Router();
+
+
+// create account api
+router.post('/createAccount', async (req, res) => {
+  try {
+    const { name, balance } = req.body; 
+
+    const bal = balance ? Number(balance) : 0.00;
+
+    const result = await pool.query(
+      'INSERT INTO accounts (name, balance) VALUES ($1, $2) RETURNING *',
+      [name, bal]
+    );
+
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error('Error creating account:', err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
+//get all accounts api
+
+router.get('/accounts', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM accounts ORDER BY account_id ASC');  
+        res.json(result.rows);
+    } catch (err) {
+        console.error('Error fetching accounts:', err);
+        res.status(500).json({ error: 'Internal server error' });
+    }       
+});
+
+//Cash in api
+router.post('/cashin', async (req, res) => {
+    try {
+        const { account_id, amount, description } = req.body;
+        
+        await pool.query('BEGIN');
+        const amt = amount ? Number(amount) : 0.00;
+
+        await pool.query(
+            'UPDATE accounts SET balance = balance + $1 WHERE account_id = $2',
+            [amt, account_id]
+        );
+
+        await pool.query(
+            'INSERT INTO transactions (to_account, amount, type, description) VALUES ($1, $2, $3, $4)',
+            [account_id, amt, 'credit', description || 'Cash In']
+        );
+        await pool.query('COMMIT');
+        res.status(200).json({ message: 'Cash in successful' });
+    } catch (err) {
+        await pool.query('ROLLBACK');
+        console.error('Error during cash in:', err);
+        res.status(500).json({ error: 'Internal server error', error: err.message });
+    }
+});
+
+
+// cash out api
+router.post('/cashout', async (req, res) => {
+    try {
+        const { account_id, amount, description } = req.body;
+
+        await pool.query('BEGIN');
+        const amt = amount ? Number(amount) : 0.00;
+
+        const acc = await pool.query("SELECT balance FROM accounts WHERE account_id=$1", [account_id]);
+
+        if (acc.rows.length === 0) throw new Error("From account not found");
+
+        if (parseFloat(acc.rows[0].balance) < amount) {
+           throw new Error("Insufficient balance");
+           } else{
+
+                await pool.query("UPDATE accounts SET balance = balance - $1 WHERE account_id=$2", 
+                    [amt, account_id]);
+
+                const txn = await pool.query(
+                  "INSERT INTO transactions (from_account, amount, type, description) VALUES ($1,$2,$3,$4) RETURNING *",
+                  [account_id, amt, 'debit', description || 'Cash Out']
+                );
+
+                await pool.query("COMMIT");
+                res.json(txn.rows[0]);
+            }
+
+        }catch (err) {
+                 await pool.query("ROLLBACK");
+                 res.status(400).json({ error: err.message })
+         } 
+});
+
+
+
+export default router;
--- backend/DB.js
+++ backend/DB.js
@@ -1,8 +1,8 @@
 import pg from 'pg';
 
-const {Pool} = pg;
+const { Pool } = pg;
 
-const pool = new Pool({
+const pool= new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'mini_ledger',
@@ -10,9 +10,10 @@ const pool = new Pool({
     port: 5432,
 });
 
-
-pool.connect().then(()=>{
-    console.log("Connected to PostgreSQL database");
-})
+pool.connect().then(() => {
+    console.log('Connected to the database successfully.');
+}).catch((err) => {
+    console.error('Database connection error:', err.stack);
+});
 
 export default pool;
